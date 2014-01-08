a678cfc9a8732e7d9875148408da0dea76df7a16
Yassine Elouazzani <yassine@youngguns.nl>
2014-01-09 00:40:14 +0100

    styling

--- background.js
+++
@@ -1,9 +0,0 @@
-//$("body").append('Test');
-//function makeYellow() {
-//    $('body').css('background', 'yellow');
-//
-//}
-
-//    $('body').click(function() {
-//       alert('ok');
-//    });
--- browseraction/popup.js
+++ browseraction/popup.js
@@ -1,20 +1,15 @@
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
-    var console = chrome.extension.getBackgroundPage().console;
+//    var console = chrome.extension.getBackgroundPage().console; //develpment
     var apiURL = 'http://youtubeinmp3.com/fetch/?video=http://www.youtube.com/watch?v=';
-//    var apiURL = 'http://www.video2mp3.net/loading.php?url=http://www.youtube.com/watch?v='+url;
     if (!youtube_parser(url)) {
         jQuery('body').addClass('no-youtube');
     }else {
-        jQuery('.loading').hide();
-        jQuery('body').addClass('download');
         jQuery('a.link').click(function() {
             chrome.tabs.create({'url': apiURL+youtube_parser(url)});
         });
     }
 });
-
-
 
 function youtube_parser(url){
     var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
