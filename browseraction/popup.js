fc34e825ccff9679e7c7635cbf7e78585001fbb5
Yassine Elouazzani <yassine@youngguns.nl>
2014-01-09 00:34:44 +0100

    styling

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -3,15 +3,14 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var console = chrome.extension.getBackgroundPage().console;
     var apiURL = 'http://youtubeinmp3.com/fetch/?video=http://www.youtube.com/watch?v=';
 //    var apiURL = 'http://www.video2mp3.net/loading.php?url=http://www.youtube.com/watch?v='+url;
-//    alert(youtube_parser(url) + jQuery('#loading').text());
     if (!youtube_parser(url)) {
         jQuery('body').addClass('no-youtube');
     }else {
+        jQuery('.loading').hide();
+        jQuery('body').addClass('download');
         jQuery('a.link').click(function() {
-            console.log(apiURL+youtube_parser(url));
-
+            chrome.tabs.create({'url': apiURL+youtube_parser(url)});
         });
-        jQuery('#loading').text(youtube_parser(url));
     }
 });
 
