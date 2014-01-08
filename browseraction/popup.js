dfab30a8fa48bc400fc964fe246ad67ce1fb9f7c
Yassine Elouazzani <yassine@youngguns.nl>
2014-01-09 00:11:18 +0100

    use the youtube2mp3 api

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -1,10 +1,18 @@
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
+    var console = chrome.extension.getBackgroundPage().console;
+    var apiURL = 'http://youtubeinmp3.com/fetch/?video=http://www.youtube.com/watch?v=';
+//    var apiURL = 'http://www.video2mp3.net/loading.php?url=http://www.youtube.com/watch?v='+url;
 //    alert(youtube_parser(url) + jQuery('#loading').text());
     if (!youtube_parser(url)) {
         jQuery('body').addClass('no-youtube');
+    }else {
+        jQuery('a.link').click(function() {
+            console.log(apiURL+youtube_parser(url));
+
+        });
+        jQuery('#loading').text(youtube_parser(url));
     }
-            jQuery('#loading').text(youtube_parser(url));
 });
 
 
